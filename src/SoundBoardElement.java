2fdd60f202c51bb6cc8cca0049fd46a1589d5c68
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-11-03 10:33:10 +0100

    built soundboard element

--- src/SoundBoardElement.java
+++ src/SoundBoardElement.java
@@ -1,19 +1,39 @@
 import de.ur.mi.oop.colors.Color;
+import de.ur.mi.oop.colors.Colors;
 import de.ur.mi.oop.graphics.GraphicsObject;
+import de.ur.mi.oop.graphics.Image;
+import de.ur.mi.oop.graphics.Label;
 import de.ur.mi.oop.graphics.Rectangle;
 
 public class SoundBoardElement extends GraphicsObject {
 
-    Rectangle background;
+    private Rectangle background;
+    private Image icon;
+    private Label text;
+    private String name;
 
-    public SoundBoardElement(float x, float y, float width, float height, Color color) {
+    public SoundBoardElement(String name, String filePath, float x, float y, float width, float height, Color color) {
         super(x, y, width, height, color);
-        background = new Rectangle(x, y, width, height, color);
+        width = width-2;
+        height = height - 2;
+        x = x + 2;
+        y = y + 2;
+        this.background = new Rectangle(x, y, width, height, color);
+        this.name = name;
+        this.text = new Label(0, height-5, name, Colors.WHITE);
+        text.setXPos(width/2 - 25);
+        this.icon = new Image(x, y, filePath);
+        icon.setHeight((height/10)*9, true);
+        icon.setWidth((width/10)*9, true);
+        icon.setXPos((width-icon.getWidth())/2);
+        icon.setYPos(((height - text.getHeightEstimate()) - icon.getHeight()) / 2);
     }
 
     @Override
     public void draw() {
         background.draw();
+        icon.draw();
+        text.draw();
     }
 
     @Override
--- src/Soundboard.java
+++ src/Soundboard.java
@@ -9,13 +9,32 @@ public class Soundboard extends GraphicsApp {
     private static final int CANVAS_WIDTH = 800;
     private static final int FRAME_RATE = 30;
 
+    // Foto von Pexels, Pixabay, Pixabay License.
+    private static final String BEAR_IMAGE_PATH = "src/img/bear.jpg";
+    // Foto von JillWellington, Pixabay, Pixabay License.
+    private static final String BIRD_IMAGE_PATH = "src/img/bird.jpg";
+    // Foto von susannp4, Pixabay, Pixabay License.
+    private static final String CAT_IMAGE_PATH = "src/img/cat.jpg";
+    // Foto von Richard655, Pixabay, Pixabay License.
+    private static final String CRICKET_IMAGE_PATH = "src/img/cricket.jpg";
+    // Foto von Chiemsee2016, Pixabay, Pixabay License.
+    private static final String DOG_IMAGE_PATH = "src/img/dog.jpg";
+    // Foto von ajoheyho, Pixabay, Pixabay License.
+    private static final String ELEFANT_IMAGE_PATH = "src/img/elefant.jpg";
+    // Foto von Chiemsee2016, Pixabay, Pixabay License.
+    private static final String HORSE_IMAGE_PATH = "src/img/horse.jpg";
+    // Foto von Robertgreene674, Pixabay, Pixabay License.
+    private static final String LION_IMAGE_PATH = "src/img/lion.jpg";
+    // Foto von PublicDomainImages, Pixabay, Pixabay License.
+    private static final String PIG_IMAGE_PATH = "src/img/pig.jpg";
+
     SoundBoardElement testElement;
 
     @Override
     public void initialize() {
         setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
         setFrameRate(FRAME_RATE);
-        testElement = new SoundBoardElement(0, 0, 20, 20, Colors.GREY);
+        testElement = new SoundBoardElement("Hund", DOG_IMAGE_PATH, 0, 0, CANVAS_WIDTH/3, CANVAS_HEIGHT/3, Colors.GREY);
     }
 
     @Override
