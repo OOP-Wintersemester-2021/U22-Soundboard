1cc96b7065829aeb96dae6e22f499c6bd11eba5e
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-11-06 16:40:13 +0100

    created soundboardview so seperate logic and view

--- src/Soundboard.java
+++ src/Soundboard.java
@@ -1,11 +1,12 @@
+import java.net.Socket;
+
 public class Soundboard {
     // Private statische Konstanten
     private static final int ELEMENTS_PER_ROW = 3;
     private static final int ELEMENTS_PER_COLUMN = 3;
 
-    // Höhe und Breite eines Soundboard Elements
-    private static float elementWidth;
-    private static float elementHeight;
+    // Das SoudboardView-Objekt kümmert sich um die Darstellung des Soundboards.
+    private SoundboardView view;
 
     // In diesem zweidimensionalen Array werden die Elemente, die gezeichnet werden sollen abgelegt.
     private SoundBoardElement[][] elements;
@@ -16,67 +17,20 @@ public class Soundboard {
 
     /*
         Die initialize-Methode wird einmalig zum Start der Anwendung ausgeführt.
-        Hier wird das zweidimensionale Array initialisiert.
+        Hier wird das zweidimensionale Array initialisiert und das View Objekt mit allen notwendigen Daten zur
+        Darstellung erzeugt.
      */
     private void initialize(int canvasWidth, int canvasHeight) {
         elements = new SoundBoardElement[ELEMENTS_PER_ROW][ELEMENTS_PER_COLUMN];
-        elementWidth = canvasWidth/ELEMENTS_PER_ROW;
-        elementHeight = canvasHeight/ELEMENTS_PER_COLUMN;
-
-        initializeElements();
-        setElementPositions();
+        view = new SoundboardView(elements, canvasWidth, canvasHeight, ELEMENTS_PER_ROW, ELEMENTS_PER_COLUMN);
     }
 
-    /*
-        In der initializeElements-Methode werden die Instanzen der SoundBoardElement-Klasse erzeugt.
-        Dabei werden neben Breite und Höhe auch der anzuzeigende Text und der Pfad zum Bild sowie der Pfad zum
-        dazu gehörigen Sound übergeben.
-        Die Elemente werden alle erstmal an der Position 0, 0 erzeugt.
-     */
-    private void initializeElements() {
-        elements[0][0] = new SoundBoardElement("Hund", Constants.DOG_IMAGE_PATH, Constants.DOG_SOUND_PATH, elementWidth, elementHeight);
-        elements[0][1] = new SoundBoardElement("Katze", Constants.CAT_IMAGE_PATH, Constants.CAT_SOUND_PATH, elementWidth, elementHeight);
-        elements[0][2] = new SoundBoardElement("Bär", Constants.BEAR_IMAGE_PATH, Constants.BEAR_SOUND_PATH, elementWidth, elementHeight);
-        elements[1][0] = new SoundBoardElement("Löwe", Constants.LION_IMAGE_PATH, Constants.LION_SOUND_PATH, elementWidth, elementHeight);
-        elements[1][1] = new SoundBoardElement("Grille", Constants.CRICKET_IMAGE_PATH, Constants.CRICKET_SOUND_PATH, elementWidth, elementHeight);
-        elements[1][2] = new SoundBoardElement("Vogel", Constants.BIRD_IMAGE_PATH, Constants.BIRD_SOUND_PATH, elementWidth, elementHeight);
-        elements[2][0] = new SoundBoardElement("Pferd", Constants.HORSE_IMAGE_PATH, Constants.HORSE_SOUND_PATH, elementWidth, elementHeight);
-        elements[2][1] = new SoundBoardElement("Schwein", Constants.PIG_IMAGE_PATH, Constants.PIG_SOUND_PATH, elementWidth, elementHeight);
-        elements[2][2] = new SoundBoardElement("Elefant", Constants.ELEFANT_IMAGE_PATH, Constants.ELEPHANT_SOUND_PATH, elementWidth, elementHeight);
-    }
+   public void draw() {
+        view.draw();
+   }
 
     /*
-        In der setElementPositions-Methode werden die Elemente an die richtige Stelle verschoben.
-        Dazu wird in einer verschachtelten for-Schleife jedes Element entsprechend seiner Position im Array richtig auf
-        dem Canvas positioniert.
-        So landet das Element an Position [0][1] an der y-Position 0*ELEMENT_HEIGHT = 0 und x-Position 1*ELEMENT_WIDTH
-        beziehungsweise das Element an der Array-Position [2][2] an der y-Position 2*ELEMENT_HEIGHT und x-Position
-        2*ELEMENT_WIDTH, dadurch überlappen sich die Elemente nicht.
-     */
-    private void setElementPositions() {
-        for (int i = 0; i < ELEMENTS_PER_COLUMN; i++) {
-            for (int j = 0; j < ELEMENTS_PER_ROW; j++) {
-                float xPos = j * elementWidth;
-                float yPos = i * elementHeight;
-                elements[i][j].setPosition(xPos, yPos);
-            }
-        }
-    }
-
-    /*
-        Hier wird jedes Element im Array einmal gezeichnet indem mit einer doppelten for-Schleife, jedes Element im
-        2D-Array einmal besucht wird.
-     */
-    public void draw() {
-        for (int i = 0; i < ELEMENTS_PER_COLUMN; i++) {
-            for (int j = 0; j < ELEMENTS_PER_ROW; j++) {
-                elements[i][j].draw();
-            }
-        }
-    }
-
-    /*
-        Dann wird überprüft, welches Element sich an der x- und y-Position des Klicks befindet und von dem entsprechenden
+        Es wird überprüft welches Element sich an der x- und y-Position des Klicks befindet und von dem entsprechenden
         Element die playSound-Mehtode aufgerufen.
         Dazu wird jedes Element im 2D-Array einmal besucht und mit hitTest überprüft, ob es getroffen wurde.
         Wurde ein passendes Element gefunden, müssen die restlichen nicht meher besucht werden. Die Methode wird mit
@@ -119,6 +73,9 @@ public class Soundboard {
 
     // Die getCurrChar-Methode gibt den ersten Buchstaben des Namens des übergebenen Elements als Kleinbuchstaben zurück.
     private char getCurrChar(SoundBoardElement soundBoardElement) {
+        if(soundBoardElement == null) {
+            return '\0';
+        }
         String currName = soundBoardElement.getName();
         char currChar = currName.charAt(0);
         currChar = Character.toLowerCase(currChar);
